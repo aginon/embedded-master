ed5e48675f077fcfde8c3f94d17d1ecbe8945df0
Kris Kalavantavanich <krismath.K@gmail.com>
2017-12-16 19:22:19 +0700

    Added gateway Errors

--- Inc/agn_errno.h
+++ Inc/agn_errno.h
@@ -11,10 +11,14 @@
 
 #include "stm32f4xx_hal.h"
 
-#define AGN_ERRNO_UNKNOWN 			1
-#define AGN_ERRNO_RESET_ERRNO 		2
-#define AGN_ERRNO_LOGGING_ERROR 	3
-#define AGN_ERRNO_LOGGING_TIMEOUT	4
+#define AGN_ERRNO_UNKNOWN 				1
+#define AGN_ERRNO_RESET_ERRNO 			2
+#define AGN_ERRNO_LOGGING_ERROR 		3
+#define AGN_ERRNO_LOGGING_TIMEOUT		4
+#define AGN_ERRNO_GATEWAY_SEND_ERROR 	5
+#define AGN_ERRNO_GATEWAY_SEND_TIMEOUT 	6
+#define AGN_ERRNO_GATEWAY_RECV_ERROR	7
+#define AGN_ERRNO_GATEWAY_RECV_TIMEOUT	8
 
 void resetErrno();
 void setErrno(int errCode);
