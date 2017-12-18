27f01fd39c9e3b35d8f4078fce47e7bbbb94dea5
Kris Kalavantavanich <krismath.K@gmail.com>
2017-12-18 23:05:14 +0700

    Update GATEWAY RESYNC mechanism

--- Components/agn_gateway.c
+++ Components/agn_gateway.c
@@ -9,6 +9,7 @@
 #include "agn_gateway.h"
 #include "agn_errno.h"
 #include "agn_logger.h"
+#include "string.h"
 
 static UART_HandleTypeDef* _gateway_huart;
 
@@ -32,6 +33,12 @@ void AGN_GATEWAY_SEND_PACKET(struct AGN_PACKET* packet) {
 	uint8_t bytes[AGN_PACKET_SIZE];
 	AGN_PACKET_SERIALIZE(packet, bytes);
 	AGN_GATEWAY_SEND_BYTES(bytes, AGN_PACKET_SIZE);
+#ifdef AGN_DEBUG
+	char str[200];
+	sprintf(str, "Sent Bytes(%d): %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x",
+			sizeof(bytes), bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7], bytes[8], bytes[9]);
+	AGN_LOG_DEBUG(str);
+#endif
 }
 
 // NOT Reassured with GATEWAY_RECONNECT
@@ -42,6 +49,18 @@ void AGN_GATEWAY_RECEIVE_BYTES(uint8_t* bytes, uint8_t size) {
 	} else if (transmitStatus == HAL_TIMEOUT) {
 		setErrno(AGN_ERRNO_GATEWAY_RECV_TIMEOUT);
 	}
+
+#ifdef AGN_DEBUG
+	char str[200];
+	char header[30] = "Received Internal:";
+	sprintf(str, "%s", header);
+	char * a = str + strlen(header);
+	for (int i = 0; i < size; i++) {
+		sprintf(a, " %02x", bytes[i]);
+		a += 3;
+	}
+	AGN_LOG_DEBUG(str);
+#endif
 }
 
 // Reassured with GATEWAY_RECONNECT
@@ -49,17 +68,12 @@ void AGN_GATEWAY_RECEIVE_PACKET(struct AGN_PACKET* packet) {
 	uint8_t bytes[AGN_PACKET_SIZE];
 	AGN_GATEWAY_RECEIVE_BYTES(bytes, AGN_PACKET_SIZE);
 	AGN_PACKET_DESERIALIZE(packet, bytes);
-	char str[200];
-	sprintf(str, "Received Bytes: 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x", bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6]);
-	AGN_LOG_DEBUG(str);
 
 	while(packet->magic != 0xA0A0) {
 
-		// Move buffer to position after magic
-		AGN_GATEWAY_RESYNC(bytes);
+		// get synced bytes
+		AGN_GATEWAY_RESYNC(bytes, AGN_PACKET_SIZE);
 
-		// Receive the rest of the bitstream
-		AGN_GATEWAY_RECEIVE_BYTES(bytes + 2, AGN_PACKET_SIZE - 2);
 		AGN_PACKET_DESERIALIZE(packet, bytes);
 	}
 }
@@ -68,7 +82,7 @@ void AGN_GATEWAY_RECEIVE_PACKET(struct AGN_PACKET* packet) {
  * sets the first `bytes` to magic
  */
 // TODO Support multiple magic
-int AGN_GATEWAY_RESYNC(uint8_t* bytes) {
+int AGN_GATEWAY_RESYNC(uint8_t* bytes, uint8_t size) {
 	uint8_t buffer;
 	uint16_t lastByte = 0x0000;
 	AGN_GATEWAY_RECEIVE_BYTES(&buffer, 1);
@@ -81,12 +95,19 @@ int AGN_GATEWAY_RESYNC(uint8_t* bytes) {
 		timer--;
 	}
 
+	bytes[0] = 0xA0;
+	bytes[1] = 0xA0;
+
+	// Receive the rest of the bitstream
+	// (** for some bizzare reason, the already read magic bytes are still read again!)
+	AGN_GATEWAY_RECEIVE_BYTES(bytes, AGN_PACKET_SIZE);
+
+	AGN_LOG_DEBUG("GATEWAY_RESYNC");
+
 	// Handle timeout error
 	if (timer == 0) {
 		return 1;
 	} else {
-		bytes[0] = 0xA0;
-		bytes[1] = 0xA0;
 		return 0;
 	}
 }
